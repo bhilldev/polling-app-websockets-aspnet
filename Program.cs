ba458615a3b577a7dde4e2746affec781a47c20b
Brandon Hill <bhilldev@outlook.com>
2025-10-20 15:19:44 -0500

    Created a basic client Razer page.

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 app.UseWebSockets();
+
 app.Map("/poll", async context =>
 {
     if (context.WebSockets.IsWebSocketRequest)
     {
         var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-        // Handle WebSocket messages (e.g., receive votes, broadcast results)
+        // Handle WebSocket messages here
     }
 });
 
-// Configure the HTTP request pipeline.
+// Configure middleware
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -35,5 +35,7 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+// Add this:
+app.MapRazorPages();
 
 app.Run();
